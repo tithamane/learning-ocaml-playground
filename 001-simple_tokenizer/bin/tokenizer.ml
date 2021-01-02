23c9887a82e719e1a1ab44e43949198f2afc736c
Thabang Thamane <thabang.biz@gmail.com>
2021-01-02 22:53:44 +0200

    Remove unneeded code in tokenizer

--- 001-simple_tokenizer/bin/tokenizer.ml
+++ 001-simple_tokenizer/bin/tokenizer.ml
@@ -5,8 +5,6 @@ let rec print_tokens (t: Parser.token list) =
     match t with
     [] -> ()
     | hd::tl ->
-        (* let _ = print_endline hd.value in *)
-
         (* "show_statement" is generated from using [@@deriving show] from  the "ppx_deriving" package.
             It is used to print types. AKA "enums" -> https://github.com/ocaml-ppx/ppx_deriving
         *)
@@ -19,17 +17,9 @@ let rec length items =
     [] -> 0
     | _::tail -> 1 + (length tail)
 
-(* let rec print_tokens token =
-    match token with
-    [] -> ()
-    | hd::tl -> 
-        let _ = print_endline hd in
-        print_tokens tl  *)
-
 let () =
     let code = In_channel.read_all "./script.thabang" in
     let tokens = Parser.parse code in
-    (* let _ = print_endline (string_of_int (length tokens)) in *)
     let _ = Printf.printf "Tokenized: %s\n" (string_of_int (length tokens)) in
     print_tokens tokens
 
